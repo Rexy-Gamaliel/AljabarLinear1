5b97c1bfabc497aab1866aef464a8c445b3c7490
Muhammad Jafar <mjafarashadiq712@gmail.com>
2020-09-24 20:30:36 +0700

    add formatting untuk splcramer

--- src/SPLcramer.java
+++ src/SPLcramer.java
@@ -1,21 +1,26 @@
 public class SPLcramer {
-    public void Solusi(Matriks matriks){
+    public void Solusi(Matriks matriks) {
+        //Menampilkan solusi Xn
+
         // cek matriks utama apakah 0
         Cramer cramer = new Cramer();
         double detUtama = cramer.Determinan(matriks, -1);
         int idxVar;
-        if(detUtama == 0){
+        if (detUtama == 0) {
             System.out.println("Determinan utama 0");
             System.out.println("Solusi tidak unik, silakan gunakan metode lain");
-        } else{
+        } else {
             System.out.println("Solusi unik : ");
             double detX;
-            for(idxVar = 0; idxVar < matriks.getCol()-1; idxVar++){
+            double X;
+            for (idxVar = 0; idxVar < matriks.getCol() - 1; idxVar++) {
                 detX = cramer.Determinan(matriks, idxVar);
-                System.out.print("X" + (idxVar+1) + " = ");
-                System.out.println(detX / detUtama);
+                System.out.print("X" + (idxVar + 1) + " = ");
+                X = detX / detUtama;
+                System.out.format("%.2f", X);
+                System.out.println();
+                //  System.out.println(detX / detUtama);
             }
         }
-
     }
 }
