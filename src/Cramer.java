1d0cac9a2a060bb3f8d83e9b15c3a7349963b808
Muhammad Jafar <mjafarashadiq712@gmail.com>
2020-09-25 14:47:55 +0700

    Perbaiki cramer.java

--- src/Cramer.java
+++ src/Cramer.java
@@ -1,74 +1,40 @@
 public class Cramer {
     public double Determinan(Matriks matriks, int variable) {
         // mengembalikan nilai determinan dengan cara cramer's rule
-
         // variable = -1, maka kembalikan determinan utama
         // variable = 0, kembalikan nilai determinan untuk mencari X1
 
-        // buat matriks dengan kolom yang ditambah /lebih besar
-        int newRow = matriks.getRow();
-        int newCol = ((matriks.getCol() - 1) * 2) - 1;
-        Matriks mainMatriks = Matriks.createMatriks(newRow, newCol);
+        double determinan;
+        if(variable == -1)
+            determinan = SPL.Determinan(matriks);
+        else{
+            int i,j;
+            // buat matriks tanpa kolom b
+            Matriks newMatriks = Matriks.createMatriks(matriks.getRow(), matriks.getCol()-1);
 
-        // copy dahulu sesuai ukuran matriks koefisien
-        int i, j;
-        for (i = 0; i < newRow; i++) {
-            for (j = 0; j < matriks.getCol(); j++) {
-                mainMatriks.setElement(i, j, matriks.getElement(i, j));
+            // siapkan array b
+            double[] columnB = new double[matriks.getRow()]; // berukuran sepanjang row
+            for(i = 0; i < matriks.getRow();i++){
+                columnB[i] = matriks.getElement( i, matriks.getCol()-1);
             }
-        }
 
-        int restCol;
-        if (variable == -1) {
-            for (i = 0; i < newRow; i++) {
-                restCol = 0;
-                for (j = matriks.getCol() - 1; j < newCol; j++) {
-                    mainMatriks.setElement(i, j, mainMatriks.getElement(i, restCol));
-                    restCol++;
+            // copy isi matriks
+            double value;
+            for(i = 0; i < matriks.getRow(); i++){
+                for (j = 0; j < matriks.getCol()-1; j++){
+                    value = matriks.getElement(i,j);
+                    newMatriks.setElement(i,j, value);
                 }
             }
-        } else {
-            // swap column b dengan idx = variable
-            mainMatriks.swapCol(variable, matriks.getCol() - 1);
 
-            // gunakan cara cramer mengisi kolom tambahan
-            for (i = 0; i < newRow; i++) {
-                restCol = 0;
-                for (j = matriks.getCol() - 1; j < newCol; j++) {
-                    mainMatriks.setElement(i, j, mainMatriks.getElement(i, restCol));
-                    restCol++;
-                }
+            // swap column b dengan colom variable
+            for(i = 0; i < newMatriks.getRow(); i++){
+                value = columnB[i];
+                newMatriks.setElement(i, variable, value);
             }
+
+            determinan = SPL.Determinan(newMatriks);
         }
-
-        // hitung determinan
-
-        //diagonal positif
-        double det = 0;
-        double mult = 1;
-        int k;
-        for (j = 0; j < matriks.getCol() - 1; j++) {
-            k = j;
-            mult = 1;
-            for (i = 0; i < mainMatriks.getRow(); i++) {
-                mult *= mainMatriks.getElement(i, k);
-                k++;
-            }
-            det += mult;
-        }
-
-        // diagonal negatif
-
-        for (j = mainMatriks.getCol() - 1; j > ((mainMatriks.getCol() / 2) - 1); j--) {
-            k = j;
-            mult = 1;
-            for (i = 0; i < mainMatriks.getRow(); i++) {
-                mult *= mainMatriks.getElement(i, k);
-                k--;
-            }
-            det -= mult;
-        }
-
-        return det;
+        return determinan;
     }
 }
