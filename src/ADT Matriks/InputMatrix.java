fd85c6bebadaae2191fdda890520360e59d3c4c8
Muhammad Jafar <mjafarashadiq712@gmail.com>
2020-09-25 17:15:53 +0700

    Menambahkan fungsi untuk membaca file di class inputMatriks

--- src/ADT Matriks/InputMatrix.java
+++ src/ADT Matriks/InputMatrix.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class InputMatrix {
@@ -5,6 +7,65 @@ public class InputMatrix {
     // InputMatrix input = new InputMatrix();
     // input.SPLrun(matriks);
 
+    public Matriks readFile() {
+        // membaca file dan memasukan nilainya ke matriks,
+        // Mengembalikan matriks augmented yang terbaca dari file
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan path dari file : ");
+        String filename = input.nextLine();
+
+        File file = new File(filename);
+        Scanner read = null;
+        try {
+            read = new Scanner(file);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Cari ada berapa baris dalam text
+        int row = 0;
+        while (read.hasNextLine()) {
+            row++;
+            read.nextLine();
+        }
+
+        Scanner readSumData = null;
+        try {
+            readSumData = new Scanner(file);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        int data = 0;
+        while (readSumData.hasNext()) {
+            data++;
+            readSumData.next();
+        }
+        int col = data / row;
+
+
+        Scanner readData = null;
+        try {
+            readData = new Scanner(file);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Matriks augmented = Matriks.createMatriks(row, col);
+        int i, j;
+        double value;
+        for (i = 0; i < augmented.getRow(); i++) {
+            for (j = 0; j < augmented.getCol(); j++) {
+                value = readData.nextDouble();
+                augmented.setElement(i, j, value);
+            }
+        }
+        System.out.println("Matriks augmented : ");
+        System.out.println(augmented);
+        return augmented;
+    }
 
     // Matriks sebelumnya sudah meminta memasukan n jumlah variabel dan m persamaan
     // nanti ukuran matriks diassign dengan
