516bec117a8cc3d5c1a6a1437899e737051e54c7
Akromi Afif <socakromiafif@gmail.com>
2020-09-30 12:47:05 +0700

    fix parametrik

--- src/SPLMatriks.java
+++ src/SPLMatriks.java
@@ -29,17 +29,17 @@ public class SPLMatriks {
             }
 
             for (int k = i + 1; k < len1D; k++) {
-                pivot = matriks.getElement(i, i + geser);
-                factor = matriks.getElement(k, i + geser) / pivot;
+                pivot = matriks.getElement(i, i+geser);
+                factor = matriks.getElement(k, i+geser) / pivot;
 
-                if (matriks.getElement(k, i + geser) != 0) {
+                if (matriks.getElement(k, i+geser) != 0) {
                     for (int j = 0; j < len2D; j++) {
                         if (geser == 1) {
-                            if (j != len2D - 1) {
-                                matriks.setElement(k, j + geser, matriks.getElement(k, j + geser) - matriks.getElement(i, j + geser) * factor);
+                            if (j != len2D-1) {
+                                matriks.setElement(k, j+geser, matriks.getElement(k, j+geser) - matriks.getElement(i, j+geser)*factor);
                             }
                         } else {
-                            matriks.setElement(k, j, matriks.getElement(k, j) - matriks.getElement(i, j) * factor);
+                            matriks.setElement(k, j, matriks.getElement(k, j) - matriks.getElement(i, j)*factor);
                         }
                     }
                 }
@@ -63,7 +63,7 @@ public class SPLMatriks {
             double factor = 0;
             if (pivot == 0) {
                 if (scanUnderPivot(matriks, i) == -1) {
-                    pivot = matriks.getElement(i, i + 1);
+                    pivot = matriks.getElement(i, i+1);
                     geser = 1;
                 } else {
                     swapRow(matriks, i, scanUnderPivot(matriks, i));
@@ -71,25 +71,25 @@ public class SPLMatriks {
                 }
             }
 
-            if (pivot != 1 && pivot != 0) {
-                for (int l = i; l < len2D; l++) {
+            if (pivot != 1 && pivot !=0) {
+                for (int l=i; l<len2D; l++) {
                     matriks.setElement(i, l, matriks.getElement(i, l) / pivot);
                 }
             }
 
             for (int k = 0; k < len1D; k++) {
                 if (pivot != 0 && k != i) {
-                    pivot = matriks.getElement(i, i + geser);
-                    factor = matriks.getElement(k, i + geser) / pivot;
+                    pivot = matriks.getElement(i, i+geser);
+                    factor = matriks.getElement(k, i+geser) / pivot;
 
-                    if (matriks.getElement(k, i + geser) != 0) {
+                    if (matriks.getElement(k, i+geser) != 0) {
                         for (int j = 0; j < len2D; j++) {
                             if (geser == 1) {
-                                if (j != len2D - 1) {
-                                    matriks.setElement(k, j + geser, matriks.getElement(k, j + geser) - matriks.getElement(i, j + geser) * factor);
+                                if (j != len2D-1) {
+                                    matriks.setElement(k, j+geser, matriks.getElement(k, j+geser) - matriks.getElement(i, j+geser)*factor);
                                 }
                             } else {
-                                matriks.setElement(k, j, matriks.getElement(k, j) - matriks.getElement(i, j) * factor);
+                                matriks.setElement(k, j, matriks.getElement(k, j) - matriks.getElement(i, j)*factor);
                             }
                         }
                     }
@@ -136,11 +136,11 @@ public class SPLMatriks {
         int len2D = matriks.getCol();
 
         int rowParametrik = -1;
-        int total = 0;
+        double total = 0;
         int totalNotZero = 0;
 
-        for (int j = len2D - 2; j >= 0; j--) {
-            if (matriks.getElement(len1D - 1, j) != 0) {
+        for (int j=len2D-2; j>=0; j--) {
+            if (matriks.getElement(len1D-1, j) != 0) {
                 totalNotZero += 1;
             }
         }
@@ -149,8 +149,8 @@ public class SPLMatriks {
             rowParametrik = -2;
         }
 
-        for (int i = len1D - 1; i >= 0; i--) {
-            for (int j = len2D - 1; j >= 0; j--) {
+        for (int i=len1D-1; i>=0; i--) {
+            for (int j=len2D-1; j>=0; j--) {
                 total += Math.abs(matriks.getElement(i, j));
             }
 
@@ -169,7 +169,7 @@ public class SPLMatriks {
         boolean isNotHaveSolution = false;
 
         if (isParametrik(matriks) == -1) {
-            isNotHaveSolution = (matriks.getElement(len1D - 1, len2D - 2) == 0);
+            isNotHaveSolution = (matriks.getElement(len1D-1, len2D-2) == 0);
         }
 
         return isNotHaveSolution;
@@ -180,8 +180,8 @@ public class SPLMatriks {
         int len2D = matriks.getCol();
         double[] coefficient = new double[len1D];
 
-        for (int i = 0; i < len1D; i++) {
-            coefficient[i] = matriks.getElement(i, len2D - 1);
+        for (int i=0; i<len1D; i++) {
+            coefficient[i] = matriks.getElement(i, len2D-1);
         }
 
         return coefficient;
@@ -198,7 +198,7 @@ public class SPLMatriks {
         double determinan = 1;
 
         if (len1D == len2D) {
-            for (int i = 0; i < len1D; i++) {
+            for (int i=0; i<len1D; i++) {
                 determinan *= matriks.getElement(i, i);
             }
         } else {
@@ -219,7 +219,7 @@ public class SPLMatriks {
         int len1D = matriks.getRow();
         int foundIdxPivot = -1;
 
-        for (int i = idx + 1; i < len1D; i++) {
+        for (int i=idx+1; i<len1D; i++) {
             if (matriks.getElement(i, idx) != 0) {
                 foundIdxPivot = i;
                 break;
@@ -234,15 +234,15 @@ public class SPLMatriks {
         int len1D = matriks.getRow();
 
         double[] hasil = new double[len1D];
-        hasil[len1D - 1] = b[len1D - 1] / matriks.getElement(len1D - 1, len1D - 1);
+        hasil[len1D-1] = b[len1D-1]/matriks.getElement(len1D-1, len1D-1);
 
-        for (int i = len1D - 2; i >= 0; i--) {
+        for (int i=len1D-2; i>=0; i--) {
             double sum = 0;
-            for (int j = i + 1; j < len1D; j++) {
-                sum += matriks.getElement(i, j) * hasil[j];
+            for (int j=i+1; j<len1D; j++) {
+                sum += matriks.getElement(i, j)*hasil[j];
             }
 
-            hasil[i] = (b[i] - sum) / matriks.getElement(i, i);
+            hasil[i]=(b[i]-sum)/matriks.getElement(i, i);
         }
 
         return hasil;
@@ -253,8 +253,8 @@ public class SPLMatriks {
         int len1D = matriks.getRow();
         int len2D = matriks.getCol();
 
-        if (rowX != len1D - 1) {
-            for (int k = 0; k < len2D; k++) {
+        if (rowX != len1D-1) {
+            for (int k=0; k<len2D; k++) {
                 double temp = matriks.getElement(rowX, k);
                 matriks.setElement(rowX, k, matriks.getElement(rowY, k));
                 matriks.setElement(rowY, k, temp);
@@ -269,11 +269,11 @@ public class SPLMatriks {
         int len1D = matriks.getRow();
         int len2D = matriks.getCol();
 
-        for (int i = 0; i < len1D; i++) {
+        for (int i=0; i<len1D; i++) {
             System.out.print("[");
             for (int j = 0; j < len2D; j++) {
-                System.out.printf("%.2f", matriks.getElement(i, j));
-                if (j != len2D - 1) {
+                System.out.printf("%.7f", matriks.getElement(i, j));
+                if (j!=len2D-1) {
                     System.out.print(", ");
                 }
             }
@@ -288,10 +288,10 @@ public class SPLMatriks {
         int len1D = matrix.length;
         System.out.print("[");
 
-        for (int i = 0; i < len1D; i++) {
+        for (int i=0; i<len1D; i++) {
             String hasilStr = new DecimalFormat("##.##").format(matrix[i]);
             System.out.print(hasilStr);
-            if (i != len1D - 1) {
+            if (i!=len1D-1) {
                 System.out.print(", ");
             }
         }
@@ -305,18 +305,18 @@ public class SPLMatriks {
         StringBuilder hasil = new StringBuilder("Solusi Parametrik: \n");
 
 //        System.out.println("Solusi Parametrik: ");
-        for (int i = 0; i < len1D; i++) {
-            for (int j = 0; j < len2D - 1; j++) {
+        for (int i=0; i<len1D; i++) {
+            for (int j=0; j<len2D-1; j++) {
                 if (matriks.getElement(i, j) != 0) {
 //                    System.out.printf("(%.2fx%d)", matriks.getElement(i, j), j+1);
 
                     hasil.append("(");
                     hasil.append(matriks.getElement(i, j));
                     hasil.append("x");
-                    hasil.append(j + 1);
+                    hasil.append(j+1);
                     hasil.append(")");
 
-                    if (j != len2D - 2) {
+                    if (j != len2D-2) {
 //                        System.out.print(" + ");
                         hasil.append(" + ");
                     } else {
@@ -331,12 +331,11 @@ public class SPLMatriks {
 
                 hasil.append(" = ");
                 hasil.append("(");
-                hasil.append(matriks.getElement(i, len2D - 1));
+                hasil.append(matriks.getElement(i, len2D-1));
                 hasil.append(")");
             }
             hasil.append("\n");
-            System.out.println();
-            ;
+            System.out.println();;
         }
 
         System.out.println(hasil);
@@ -353,10 +352,10 @@ public class SPLMatriks {
         StringBuilder hasil = new StringBuilder("Solusi Tunggal: \n");
 //        System.out.println("Solusi Tunggal: ");
 
-        for (int i = 0; i < len1D; i++) {
-            if (i != len1D - 1) {
+        for (int i=0; i<len1D; i++) {
+            if (i != len1D-1) {
                 hasil.append("x");
-                hasil.append(i + 1);
+                hasil.append(i+1);
                 hasil.append(" = ");
                 hasil.append(matrix[i]);
                 hasil.append(" ");
@@ -364,7 +363,7 @@ public class SPLMatriks {
 //                System.out.printf("x%d = %.5f, ", i+1, matrix[i]);
             } else {
                 hasil.append("x");
-                hasil.append(i + 1);
+                hasil.append(i+1);
                 hasil.append(" = ");
                 hasil.append(matrix[i]);
                 hasil.append(" ");
